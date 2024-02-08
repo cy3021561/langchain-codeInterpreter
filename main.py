63bf299d6d5250b6f59004aa9c0a1b0bea768ec4
cy3021561 <cyang513@usc.edu>
2024-02-08 13:17:50 -0800

    add route agent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import dotenv
-from langchain.agents import AgentType
+from langchain.agents import AgentType, initialize_agent
 from langchain_community.chat_models import ChatOpenAI
+from langchain_core.tools import Tool
 from langchain_experimental.agents.agent_toolkits.python.base import create_python_agent
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_experimental.tools.python.tool import PythonREPLTool
 
 dotenv.load_dotenv()
@@ -16,10 +18,41 @@ def main():
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True,
     )
-    python_agent_executor.run(
-        """Generate 5 QRcode that point to www.yahoo.com and save them in current working directory, \
+
+    csv_agent = create_csv_agent(
+        ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+        "episode_info.csv",
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+    )
+
+    grand_agent = initialize_agent(
+        tools=[
+            Tool(
+                name="PythonAgent",
+                func=python_agent_executor.run,
+                description="""useful when you need to transform natural language and write from it python and execute the python code,
+                              returning the results of the code execution,
+                            DO NOT SEND PYTHON CODE TO THIS TOOL""",
+            ),
+            Tool(
+                name="CSVAgent",
+                func=csv_agent.run,
+                description="""useful when you need to answer question over episode_info.csv file,
+                             takes an input the entire question and returns the answer after running pandas calculations""",
+            ),
+        ],
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+        verbose=True,
+    )
+
+    grand_agent.run(
+        """Generate 5 QRcode that point to www.google.com and save them in current working directory, \
         you have the qrcode package installed already."""
     )
+
+    grand_agent.run("print seasons ascending order of the number of episodes they have")
 
 
 if __name__ == "__main__":
